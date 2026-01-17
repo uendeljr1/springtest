127673e871b43cc3507aa9c942e9aed55757db87
Uendel Junior <uendeljr1@gmail.com>
2026-01-17 13:11:18 -0300

    Aula 3

--- src/main/java/org/example/test_spring/entities/User.java
+++ src/main/java/org/example/test_spring/entities/User.java
@@ -1,11 +1,15 @@
 package org.example.test_spring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
