6460789bc5a5bc3d867d1a4f4d2bb578f93c0767
Uendel Junior <uendeljr1@gmail.com>
2026-01-20 18:04:23 -0300

    Aula 7

---
+++ src/main/java/org/example/test_spring/entities/enums/OrderStatus.java
@@ -0,0 +1,27 @@
+package org.example.test_spring.entities.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT(1),
+    PAID(2),
+    SHIPPED(3),
+    DELIVERED(4),
+    CANCELLED(5);
+    private int code;
+    private OrderStatus(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static OrderStatus valueOf(int code) {
+        for (OrderStatus orderStatus : OrderStatus.values()) {
+            if (orderStatus.getCode() == code) {
+                return orderStatus;
+            }
+
+        }
+        throw new IllegalArgumentException("Invalid OrderStatus code:  " + code);
+    }
+}
