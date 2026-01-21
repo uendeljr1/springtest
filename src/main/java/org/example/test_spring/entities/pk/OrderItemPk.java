6c3d57dfd12ba9b590bf266798277f51da59224f
Uendel Junior <uendeljr1@gmail.com>
2026-01-21 11:24:52 -0300

    Aula 10 "Parte 1"

---
+++ src/main/java/org/example/test_spring/entities/pk/OrderItemPk.java
@@ -0,0 +1,52 @@
+package org.example.test_spring.entities.pk;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import org.example.test_spring.entities.Order;
+import org.example.test_spring.entities.Product;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemPk that = (OrderItemPk) o;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
